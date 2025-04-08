3b437fb2930c21c3baf7c83f7b61d691e9579e13
Stefan Ziegler <edi.gonzales@gmail.com>
2025-04-08 17:07:51 +0200

    initial commit

---
+++ src/main/java/ch/so/agi/iliformat/ImportInterlis.java
@@ -0,0 +1,18 @@
+package ch.so.agi.iliformat;
+
+import java.io.File;
+
+import ch.interlis.ili2c.Ili2cException;
+import ch.interlis.ili2c.config.*;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+
+import ch.ehi.basics.logging.EhiLogger;
+
+public class ImportInterlis {
+    
+    public static void readIliFile(File iliFiles[]) {
+        TransferFromIli2cMetamodel convert = new TransferFromIli2cMetamodel();
+    }
+
+    
+}
