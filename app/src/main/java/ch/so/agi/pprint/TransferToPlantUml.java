79e9923afaedaf2a8173166d6f5695986b4006da
Stefan Ziegler <edi.gonzales@gmail.com>
2025-06-01 18:22:43 +0200

    clean code

--- app/src/main/java/ch/so/agi/pprint/TransferToPlantUml.java
+++ app/src/main/java/ch/so/agi/pprint/TransferToPlantUml.java
@@ -15,7 +15,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.io.OutputStream; 
 
 import ch.ehi.interlis.associations.AssociationDef;
